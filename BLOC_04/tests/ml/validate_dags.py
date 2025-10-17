312b78c17bd4efb8ab346fd2d616d7bef1bd084f
eric-nguyen-jedha <skincare.jedha@gmail.com>
2025-10-17 20:59:19 +0200

    Update validate_dags.py

--- BLOC_04/tests/ml/validate_dags.py
+++ BLOC_04/tests/ml/validate_dags.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 
 # Ajouter le répertoire des DAGs au path
-dags_dir = Path(__file__).parent.parent / 'dags'
+dags_dir = Path(__file__).parent.parent.parent / 'dags_ml'
 sys.path.insert(0, str(dags_dir))
 
 def validate_dag_file(dag_file):
